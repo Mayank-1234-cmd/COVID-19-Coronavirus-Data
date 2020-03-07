99376094fc6f66c08da87ad6bfeb9ae586b7dbb0
Mayank-1234-cmd <54957445+Mayank-1234-cmd@users.noreply.github.com>
2020-03-07 13:07:39 -0500

    it actually works now

--- country_sitreps/CountrySitrepViewer.bat
+++ country_sitreps/CountrySitrepViewer.bat
@@ -3,7 +3,7 @@ color 0b
 title Country COVID-19 Sitrep Viewer
 cls
 echo Which date do you want to view it in (march-5-2020,march-6-2020, etc...)? 
-set /p chc=>
+set /p chc=Choice:
 type %chc%.dll
 :l
 pause
